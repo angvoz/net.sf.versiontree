8563e266a07665723ea84cab5c606bf3877f55d1
Olexiy Buyanskyy <olexiyb@gmail.com>
2010-05-12 20:38:11 +0000

    removed french symbols

--- VersionTree/src/net/sf/versiontree/data/BranchTree.java
+++ VersionTree/src/net/sf/versiontree/data/BranchTree.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2003 Jan Karstens, André Langhorst.
+ * Copyright (c) 2003 Jan Karstens, Andr Langhorst.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
- *     André Langhorst <andre@masse.de> - initial implementation
+ *     Andr Langhorst <andre@masse.de> - initial implementation
  *     Jan Karstens <jan.karstens@web.de> - extensions
  *******************************************************************************/
 package net.sf.versiontree.data;
