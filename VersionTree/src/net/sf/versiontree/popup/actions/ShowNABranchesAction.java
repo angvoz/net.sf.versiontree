61e69f98024094ea3cb265cdd81b2d2a3f6ed6f4
Andrew Gvozdev <angvoz.dev@gmail.com>
2012-07-06 00:28:02 -0400

    Refined wording: n/a branch -> unnamed

--- VersionTree/src/net/sf/versiontree/popup/actions/ShowNABranchesAction.java
+++ VersionTree/src/net/sf/versiontree/popup/actions/ShowNABranchesAction.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2003 Jan Karstens, André Langhorst.
- * All rights reserved. This program and the accompanying materials 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
- * 
+ *
  * Contributors:
  *     Jan Karstens <jan.karstens@web.de> - initial implementation
  *******************************************************************************/
@@ -15,34 +15,37 @@ import net.sf.versiontree.ui.TreeViewConfig;
 import net.sf.versiontree.views.VersionTreeView;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
 
 /**
  * @author Olexiy
  * Action that toggles the visiblity of n/a branches.
  */
 public class ShowNABranchesAction extends Action {
-	
+
 	private VersionTreeView viewPart;
 
 	private TreeViewConfig treeViewConfig;
 
 	public ShowNABranchesAction(VersionTreeView theViewPart, TreeView theView) {
-		super("Display branches without name", Action.AS_CHECK_BOX);
+		super("Display branches without name", IAction.AS_CHECK_BOX);
 		treeViewConfig = theView.getTreeViewConfig();
 		viewPart = theViewPart;
 	}
 
+	@Override
 	public void run() {
 		treeViewConfig.setDrawNABranches(isChecked());
 		viewPart.renderCurrentVersionTree();
 	}
 
+	@Override
 	public void setChecked(boolean checked) {
 		super.setChecked(checked);
 		if (checked) {
-			setToolTipText("Hide n/a branches");
+			setToolTipText("Hide unnamed branches");
 		} else {
-			setToolTipText("Display n/a Branches");
+			setToolTipText("Display unnamed Branches");
 		}
 	}
 
